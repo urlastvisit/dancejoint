26d7cefb362d755bca63b9ed75b51e0e2328be33
urlastvisit <masterplanthaku18@gmail.com>
2018-08-10 20:01:54 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url =require('url');
 
 var server = http.createServer(function(req,res){
-    var page =uls.parse(req.url).pathname;
+    var page =url.parse(req.url).pathname;
     console.log(page);
     res.writeHead(200,{"content-type":"text/plan"});
     res.write('well Hello');
