1c8defd3ad2f398dec40d0392530f267e4dc2122
urlastvisit <masterplanthaku18@gmail.com>
2018-08-10 20:32:22 +0530

    first commit

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var server = http.createServer(function(req,res){
     var page =url.parse(req.url).pathname;
     console.log(page);
     res.writeHead(200,{"content-type":"text/plan"});
-    res.write('Hello welcome to dancejoint');
-    res.write('we are under construction');
-    res.write("we'll be live soon" );
+    res.write('Hello welcome to dancejoint ');
+    res.write('We are under maintainance ');
+    res.write("We'll be live soon" );
     res.end();
 });
 server.listen(4200);
