2240661af1de68520aec5c263ecaeae1bf4ff362
urlastvisit <masterplanthaku18@gmail.com>
2018-08-10 12:30:52 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
-var instructionsNewVisitor = http.createServer(function(req, res){
- res.writeHead(200);
- res.end('Hi welcome to danceJoint');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
 });
-var server=http.createServer(instructionsNewVisitor);
-server.listen(8080);
+server.listen(80);
